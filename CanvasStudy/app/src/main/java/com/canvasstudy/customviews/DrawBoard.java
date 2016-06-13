7b64368e55201742df019a85b2417f93adb446cb
supermanmwg <554607289@qq.com>
2016-06-13 08:08:38 +0800

    it doesn't work now

--- CanvasStudy/app/src/main/java/com/canvasstudy/customviews/DrawBoard.java
+++ CanvasStudy/app/src/main/java/com/canvasstudy/customviews/DrawBoard.java
@@ -9,6 +9,7 @@ import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -16,10 +17,14 @@ import android.view.View;
  * Created by mwg on 16-6-12.
  */
 public class DrawBoard extends View {
+    private static final String TAG = "DrawBoard";
     private Paint mPaint;
+    private Paint mPaint2;
     private Path mPath;
+    private Path mPath2;
     private Canvas mCanvas;
     private Bitmap mBitmap;
+    private int style = 1;
     public DrawBoard(Context context) {
         this(context, null);
     }
@@ -39,27 +44,23 @@ public class DrawBoard extends View {
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeWidth(10);
         mPath = new Path();
+
+        mPaint2 = new Paint();
+        mPaint2.setAntiAlias(true);
+       // mPaint2.setAlpha(0);
+      //  mPaint2.setColor(Color.WHITE);
+        mPaint2.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST));
+        mPaint2.setStyle(Paint.Style.STROKE);
+        mPaint2.setStrokeWidth(30);
+        mPath2 = new Path();
         mBitmap = Bitmap.createBitmap(400, 600, Bitmap.Config.ARGB_8888);
         mCanvas = new Canvas(mBitmap);
 
     }
 
       public void setPaint(int style) {
-          if(style == 1) {
-              mPath.reset();
-              mPaint.reset();
-              mPaint.setAntiAlias(true);
-              mPaint.setStyle(Paint.Style.STROKE);
-              mPaint.setStrokeWidth(10);
-          } else if(style == 0) {
-              mPaint.reset();
-              mPaint.setAntiAlias(true);
-              mPaint.setAlpha(0);
-              mPaint.setColor(Color.WHITE);
-              mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
-              mPaint.setStyle(Paint.Style.STROKE);
-              mPaint.setStrokeWidth(10);
-          }
+         this.style = style;
+          Log.d(TAG, "style is " + style);
       }
 
     @Override
@@ -70,11 +71,21 @@ public class DrawBoard extends View {
 
         switch (action) {
             case MotionEvent.ACTION_DOWN:
-                mPath.moveTo(x, y);
+                if(style == 1) {
+                    mPath.moveTo(x, y);
+                } else {
+                    mPath2.moveTo(x,y);
+                }
                 break;
             case MotionEvent.ACTION_MOVE:
-                mPath.lineTo(x, y);
-                mCanvas.drawPath(mPath, mPaint);
+                if(style == 1) {
+                    mPath.lineTo(x, y);
+                    mCanvas.drawPath(mPath, mPaint);
+                } else {
+                    Log.d(TAG, "0");
+                    mPath2.lineTo(x, y);
+                    mCanvas.drawPath(mPath2, mPaint2);
+                }
                 invalidate();
                 break;
             case MotionEvent.ACTION_UP:
